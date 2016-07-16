1409609c2197b1547555f96186811df9e6f80427
chowjune37 <chowjune37@gmail.com>
2016-07-16 17:03:56 +0800

    chang loader.asm learn gdt number

--- learnsys/loader.asm
+++ learnsys/loader.asm
@@ -1,7 +1,16 @@
 org 0100h
 jmp LABEL_BEGIN
+%include "pm.inc"
 
 [SECTION .gdt]
+LABEL_GDT:		Descriptor	0,	0,	0
+LABEL_DESC_CODE32:	Descriptor	0,	0,	0
+
+GdtLen equ $ - LABEL_GDT
+GdtPtr	dw	GdtLen - 1
+	dd	090000h + LABEL_GDT
+
+SelectorCode32	equ	LABEL_DESC_CODE32 - LABEL_GDT
 
 [SECTION .b16]
 [BITS 16]
