3376da41aa8a0adb38fc36772206d29722e9014e
chowjune37 <chowjune37@gmail.com>
2016-07-22 14:04:26 +0800

    tss

--- learnsys/loader.asm
+++ learnsys/loader.asm
@@ -8,6 +8,7 @@ LABEL_DESC_CODE32:	Descriptor	0,		Code32Len - 1,	4098h
 LABEL_DESC_VIODE:	Descriptor	0B8000h,	0FFFFFh,	92h
 LABEL_DESC_STACK32:	Descriptor	0,		StackOfTop,	4092h
 LABEL_DESC_DATA32:	Descriptor	0,		Data32Len - 1,	92h
+LABEL_DESC_TSS:		Descriptor	0,		TSSLen - 1,	89h
 
 GdtLen equ $ - LABEL_GDT
 GdtPtr	dw	GdtLen - 1
@@ -17,6 +18,38 @@ SelectorCode32	equ	LABEL_DESC_CODE32 - LABEL_GDT
 SelectorViode	equ	LABEL_DESC_VIODE  - LABEL_GDT
 SelectorStack32	equ	LABEL_DESC_STACK32- LABEL_GDT
 SelectorData32	equ	LABEL_DESC_DATA32 - LABEL_GDT
+SelectorTss	equ	LABEL_DESC_TSS    - LABEL_GDT
+
+[SECTION .tss]
+LABEL_SEG_TSS:
+	dd	0	;stackoftop0
+	dd	0	;stack0
+	dd	0	;stackoftop1
+	dd	0	;stack1
+	dd	0	;stackoftop2
+	dd	0	;stack2
+	dd	0	;cr3
+	dd	0	;eip
+	dd	0	;eflag
+	dd	0	;eax
+	dd	0	;ecx
+	dd	0	;edx
+	dd	0	;ebx
+	dd	0	;esp
+	dd	0	;ebp
+	dd	0	;esi
+	dd	0	;ebi
+	dd	0	;es
+	dd	0	;cs
+	dd	0	;ss
+	dd	0	;ds
+	dd	0	;fs
+	dd	0	;gs
+	dd	0	;ldt
+	dw	0
+	dw	$ - LABEL_SEG_TSS + 2	;
+	db	0FFh	;
+TSSLen	equ	$ - LABEL_SEG_TSS
 
 [SECTION .s32]
 [BITS 32]
@@ -68,6 +101,15 @@ LABEL_BEGIN:
 	mov byte [LABEL_DESC_DATA32 + 4],al
 	mov byte [LABEL_DESC_DATA32 + 7],ah
 
+	xor eax,eax
+	mov ax,cs
+	shl eax,4
+	add eax,LABEL_SEG_TSS
+	mov word [LABEL_DESC_TSS + 2],ax
+	shr eax,16
+	mov byte [LABEL_DESC_TSS + 4],al
+	mov byte [LABEL_DESC_TSS + 7],ah
+
 	lgdt [GdtPtr]
 
 	cli
@@ -94,6 +136,9 @@ LABEL_SEG_CODE32:
 	mov esp,StackOfTop
 	mov ax,SelectorViode
 	mov gs,ax
+
+	mov ax,SelectorTss
+	ltr ax
 
 	mov ecx,10
 	xor esi,esi
